6353be81b2685915c28c55d7783d652c944001d7
Al-Shouha Modafar <modafar.al-shouha@nokia.com>
2021-03-12 13:02:44 +0100

    fix bug "tf.contrib.slim"

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division
 import os,time,cv2, sys, math
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-import tensorflow.contrib.slim as slim
+import tf.contrib.slim as slim
 import numpy as np
 import time, datetime
 import os, random
